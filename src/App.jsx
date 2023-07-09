c0e1c81b687de84473b569a08944112ade6cb132
MmmarRTHa <m27842n@outlook.com>
2023-07-09 15:33:02 -0700

    - update: main - added state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react'
 import Header from './components/Header'
 
 function App() {
+    const [amount, setAmount] = useState(10000);
+
+    function handleChange(e) {
+        setAmount(+e.target.value)
+    }
+
 
   return (
-    <div className="my-20 max-w-2xl mx-auto bg-white rounded-lg shadow-lg shadow-slate-400 p-10"> 
+    <div className="my-20 max-w-2xl mx-auto bg-white rounded-lg shadow-lg shadow-slate-50 p-10"> 
 	  	<Header /> 
 
         <input 
             type="range"
-            className="w-full h-6 bg-gray-200 accent-pink-500 hover:accent-pink-600" 
+            className="w-full h-6 bg-gray-200 accent-purple-500 hover:accent-purple-600"
+            onChange={ handleChange } 
         /> 
+
+        <p className='text-center my-10 text-5xl font-extrabold text-purple-700'> $ {amount}</p>
     </div>
   )
 }
