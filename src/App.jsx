27e9a49231e29ade8b8d71d47854c69cfc0f5652
MmmarRTHa <m27842n@outlook.com>
2023-07-09 00:49:16 -0700

    - update: - main - added first component header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Header from './components/Header'
+
 function App() {
+
   return (
-    <h1 className="text-6xl font-extrabold">Hello, world!</h1>
+    <div className="my-20 max-w-2xl mx-auto bg-white rounded-lg shadow-lg shadow-slate-400 p-10"> 
+	  	<Header /> 
+
+        <input 
+            type="range"
+            className="w-full h-6 bg-gray-200 accent-pink-500 hover:accent-pink-600" 
+        /> 
+    </div>
   )
 }
 
