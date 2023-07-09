304ce778649266ed4f5ca518cadb0fa1b009c0d7
MmmarRTHa <m27842n@outlook.com>
2023-07-09 15:59:47 -0700

    - update: main - added max, min and step variables.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import Header from './components/Header'
 
 function App() {
-    const [amount, setAmount] = useState(10000);
+    const [amount, setAmount] = useState(25000);
+
+    const MIN = 0;
+    const MAX = 50000;
+    const STEP = 100;
 
     function handleChange(e) {
         setAmount(+e.target.value)
@@ -17,6 +21,10 @@ function App() {
             type="range"
             className="w-full h-6 bg-gray-200 accent-purple-500 hover:accent-purple-600"
             onChange={ handleChange } 
+            min={ MIN }
+            max={ MAX }
+            step={ STEP }
+            value={ amount }
         /> 
 
         <p className='text-center my-10 text-5xl font-extrabold text-purple-700'> $ {amount}</p>
