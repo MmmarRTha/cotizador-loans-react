3eec67eba93884c47a4203ef5f47714b608f8221
MmmarRTHa <m27842n@outlook.com>
2023-07-09 23:27:13 -0700

    - update: main - added final changes to UI.

--- src/App.jsx
+++ src/App.jsx
@@ -39,57 +39,52 @@ function App() {
     }
 
   return (
-    <div className="max-w-2xl p-10 mx-auto my-20 bg-white rounded-lg shadow-lg shadow-slate-50"> 
-	  	<Header /> 
-
-        <div className='flex justify-between my-6'>
-            <Button
-                operator='-' 
-                fn={ handleClickDecrement }
-            />
-            <Button
-                operator='+' 
-                fn={ handleClickIncrement }
-            />
-        </div>
-
-        <input 
-            type="range"
-            className="w-full h-6 bg-gray-200 accent-purple-500 hover:accent-purple-600"
-            onChange={ handleChange } 
-            min={ MIN }
-            max={ MAX }
-            step={ STEP }
-            value={ amount }
-        /> 
-
-        <p className='my-10 text-5xl font-extrabold text-center text-fuchsia-700'> 
-           {moneyFormat(amount)}
-        </p>
-
-        <h2 className='text-2xl font-extrabold text-center text-gray-500'>
-            <span className='text-fuchsia-600'>Plazo</span> a pagar
-        </h2>
-
-        <select 
-            className='w-full p-2 mt-5 text-xl font-bold text-center text-gray-500 bg-white border border-gray-300 rounded-lg'
-            value={ months }
-            onChange={ e => setMonths(+e.target.value)}
-        >
-            <option value="6">6 meses</option>
-            <option value="12">12 meses</option>
-            <option value="24">24 meses</option>
-        </select>
-
-        <div className='p-5 my-5 space-y-3 bg-gray-100 rounded-xl'>
+    <><div>
+          <h1 className='mt-20 text-6xl text-center text-black'>Préstamos rápidos</h1>
+      </div>
+        <div className="max-w-2xl p-10 mx-auto my-20 bg-white rounded-lg shadow-lg shadow-slate-50">
+            <Header />
+            <div className='flex justify-between my-6'>
+                <Button
+                    operator='-'
+                    fn={handleClickDecrement} />
+                <Button
+                    operator='+'
+                    fn={handleClickIncrement} />
+            </div>
+            <input
+                type="range"
+                className="w-full h-6 bg-gray-200 accent-purple-500 hover:accent-purple-600"
+                onChange={handleChange}
+                min={MIN}
+                max={MAX}
+                step={STEP}
+                value={amount} />
+            <p className='my-10 text-5xl font-extrabold text-center text-fuchsia-700'>
+                {moneyFormat(amount)}
+            </p>
             <h2 className='text-2xl font-extrabold text-center text-gray-500'>
-                Resumen <span className='text-fuchsia-600'>de pagos</span>
+                <span className='text-fuchsia-600'>Plazo</span> a pagar
             </h2>
-            <p className='text-xl font-bold text-center text-gray-500'>{months} Meses</p>
-            <p className='text-xl font-bold text-center text-gray-500'>{moneyFormat(total)} Total a pagar</p>
-            <p className='text-xl font-bold text-center text-gray-500'>{moneyFormat(payment)} Mensuales</p>
-        </div>
-    </div>
+            <select
+                className='w-full p-2 mt-5 text-xl font-bold text-center text-gray-500 border rounded-lg border-slate-200 bg-slate-100'
+                value={months}
+                onChange={e => setMonths(+e.target.value)}
+            >
+                <option value="6">6 meses</option>
+                <option value="12">12 meses</option>
+                <option value="24">24 meses</option>
+            </select>
+
+            <div className='p-5 my-5 space-y-3 bg-slate-100 rounded-xl'>
+                <h2 className='text-2xl font-extrabold text-center text-gray-500'>
+                    Resumen <span className='text-fuchsia-600'>de pagos</span>
+                </h2>
+                <p className='text-xl font-bold text-center text-gray-500'>{months} Meses</p>
+                <p className='text-xl font-bold text-center text-gray-500'>{moneyFormat(total)} Total a pagar</p>
+                <p className='text-xl font-bold text-center text-gray-500'>{moneyFormat(payment)} Mensuales</p>
+            </div>
+        </div></>
   )
 }
 
